24e6f344454b3283ae85e75781dd34d57a8a1b3d
Diego Saavedra <dsaavedra88@gmail.com>
2022-11-08 16:56:46 -0500

    Correcciones y Sugerencias

--- Operadores.py
+++ Operadores.py
@@ -12,6 +12,13 @@ print(list[0])
 
 list.append("Diego")
 
-list2 = list.copy
+list2 = list.copy()
+print(list2)
+list.clear()
+print(list)
 
-list.clear
+"""
+Recuerda que los comentarios son notas dirigidas para tí mismo o para 
+personas que lleguen a revisar tu código, por lo tanto hay que hacer
+un pequeño esfuerzo de poder generar scripts de desarrollo lo mayor entendibles posibles
+"""
